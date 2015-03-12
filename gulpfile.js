3a5647b18b1ccc9afd77acabf1fce2a2c60e9ddf
Nick Reid <nickreid@nickreid.com>
2015-03-12 00:29:31 -0400

    added front end framework

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'), 
+    sass = require('gulp-ruby-sass') 
+    notify = require("gulp-notify") 
+    bower = require('gulp-bower');
+
+var config = {
+     sassPath: './resources/sass',
+     bowerDir: './bower_components' 
+}
+
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
+
+gulp.task('icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('./mmch_pilot/static/fonts')); 
+});
+
+gulp.task('css', function() { 
+    return sass(config.sassPath + '/styles.scss', {
+             style: 'compressed',
+             loadPath: [
+                 './resources/sass',
+                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+                 config.bowerDir + '/fontawesome/scss',
+             ]
+         }) 
+         .pipe(gulp.dest('./mmch_pilot/static/css')); 
+});
+
+// Rerun the task when a file changes
+ gulp.task('watch', function() {
+     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+});
+
+  gulp.task('default', ['bower', 'icons', 'css']);
