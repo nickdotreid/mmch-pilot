4c8accc23781142f973bc2eee26a19c536a65e77
Nick Reid <nickreid@nickreid.com>
2015-03-15 19:03:42 -0400

    added question detail

--- questions/urls.py
+++ questions/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('questions.views',
+	url(r'^(?P<question_id>\d+)','detail', name='questions_detail'),
 	url(r'^','list', name='questions_list'),
 	)
--- questions/views.py
+++ questions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.template import RequestContext 
 
@@ -8,3 +8,10 @@ def list(request):
 	return render_to_response('questions/list.html',{
 		'questions':Question.objects.all(),
 		}, context_instance = RequestContext(request))
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render_to_response('questions/detail.html',{
+		'question':question,
+		'answers':question.answers.all(),
+		}, context_instance = RequestContext(request))	
