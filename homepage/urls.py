bd6ba3dd5f418d51cf7a4e1b445bb08525adfbc1
Nick Reid <nickreid@nickreid.com>
2015-03-15 17:22:36 -0400

    added user register screen

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('homepage.views',
+	url(r'^register$','register', name='register'),
 	url(r'^login$','login', name='login'),
 	url(r'^','home', name='home'),
 	)
--- homepage/views.py
+++ homepage/views.py
@@ -5,7 +5,9 @@ from django.template import RequestContext
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from phonenumber_field.formfields import PhoneNumberField
 
+from sms.models import Number
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login
 
@@ -20,6 +22,18 @@ class LoginForm(forms.Form):
         self.form_action = reverse(login)
 
         self.helper.add_input(Submit('submit', 'Login'))
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField()
+    phone_number = PhoneNumberField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.form_action = reverse(register)
+
+        self.helper.add_input(Submit('submit', 'Register'))
 
 
 # Create your views here.
@@ -49,5 +63,41 @@ def login(request):
             except:
                 pass
     return render_to_response('main/login.html',{
+        'form':form,
+        }, context_instance = RequestContext(request))
+
+def register(request):
+    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+        if(form.is_valid()):
+            if User.objects.filter(email=form.cleaned_data['email']).exists():
+                # Set message saying email exists
+                pass
+            elif Number.objects.filter(phone_number=form.cleaned_data['phone_number']).exists():
+                # Set message saying phone number exists
+                pass
+            else:
+                user = User(
+                    username = form.cleaned_data['email'],
+                    email = form.cleaned_data['email'],
+                    )
+                user.set_password(form.cleaned_data['password'])
+                user.save()
+
+                number = Number(
+                    phone_number = form.cleaned_data['phone_number'],
+                    user = user,
+                    )
+                number.save()
+                user = authenticate(
+                    username = form.cleaned_data['email'],
+                    password = form.cleaned_data['password'],
+                    )
+                if user:
+                    auth_login(request, user)
+                    return redirect(reverse('home'))
+                # add message that something went wrong
+    return render_to_response('main/register.html',{
         'form':form,
         }, context_instance = RequestContext(request))
