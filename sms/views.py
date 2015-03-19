9128c6c02b9a1793766f0fa7916915800e67a434
Nick Reid <nickreid@nickreid.com>
2015-03-19 13:00:52 -0400

    attempting to tie together service

--- sms/views.py
+++ sms/views.py
@@ -3,18 +3,23 @@ from django_twilio.decorators import twilio_view
 from django_twilio.request import decompose
 
 from sms.models import Number
+from questions.models import Question
 
 @twilio_view
 def gateway(request):
     twilio_request = decompose(request)
     # if number isn't associated with a user - ask them to register first
     try:
-        user = Number.objects.get(phone_number=twilio_request.get['From',none]).first().user
+        user = Number.objects.get(phone_number=twilio_request.get('From',none)).first().user
     except:
         r = twiml.Response()
         r.message('We dont recognize your number - please register online.')
         return r
-    # treat message as question
+    question = Question(
+        text = twilio_request.get('Body',none),
+        user = user,
+        )
+    question.save()
     r = twiml.Response()
-    r.message('Thanks for the SMS message!')
+    r.message('Your message has been saved')
     return r
