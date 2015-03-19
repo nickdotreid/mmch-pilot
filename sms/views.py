cdc2534ff949882b1c1917ad2c53386b7fb431ac
Nick Reid <nickreid@nickreid.com>
2015-03-19 12:54:12 -0400

    changed phone number so this works right

--- sms/views.py
+++ sms/views.py
@@ -9,7 +9,7 @@ def gateway(request):
     twilio_request = decompose(request)
     # if number isn't associated with a user - ask them to register first
     try:
-        user = Number.objects.get(number=twilio_request.get['From',none]).first().user
+        user = Number.objects.get(phone_number=twilio_request.get['From',none]).first().user
     except:
         r = twiml.Response()
         r.message('We dont recognize your number - please register online.')
