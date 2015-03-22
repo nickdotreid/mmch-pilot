c3cbd7876fbfb5f8a1d753012f851e874a914461
Nick Reid <nickreid@nickreid.com>
2015-03-22 19:33:13 -0400

    added terminal view, which has form to fake-send & recieve sms messages

--- sms/urls.py
+++ sms/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('sms.views',
+	url(r'^terminal/(?P<number>.+)','terminal', name='sms_terminal'),
+	url(r'^terminal','terminal', name='sms_terminal'),
 	url(r'^','gateway', name='sms_gateway'),
 	)
--- sms/views.py
+++ sms/views.py
@@ -1,9 +1,38 @@
+from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+
 from twilio import twiml
 from django_twilio.decorators import twilio_view
 from django_twilio.request import decompose
 
 from sms.models import Number
 from questions.models import Question
+
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+from homepage.views import CustomPhoneNumber
+
+class TerminalForm(forms.Form):
+    message = forms.CharField()
+    phone_number = CustomPhoneNumber()
+
+    def __init__(self, *args, **kwargs):
+        self.number = kwargs.pop('number', None)
+        super(TerminalForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        if self.number:
+            self.initial = {
+                'phone_number':self.number,
+            }
+            self.fields['phone_number'].widget = forms.HiddenInput()
+            # set and hide phone number input
+            self.helper.form_action = reverse(terminal, kwargs={
+                'number':self.number,
+                })
+        self.helper.add_input(Submit('submit', 'Send Message'))
 
 @twilio_view
 def gateway(request):
@@ -24,3 +53,20 @@ def gateway(request):
     r = twiml.Response()
     r.message('Your message has been saved')
     return r
+
+def terminal(request, number=None):
+    if number:
+        number = get_object_or_404(Number, phone_number=number).phone_number.as_e164
+    form = TerminalForm(number=number)
+    if request.POST:
+        form = TerminalForm(request.POST, number=number)
+        if form.is_valid():
+            number, created = Number.objects.get_or_create(
+                phone_number=form.cleaned_data['phone_number']
+                )
+            return redirect(reverse(terminal, kwargs={
+                'number':number.phone_number.as_e164,
+                }))
+    return render_to_response('sms/terminal.html',{
+        'form': form,
+        }, context_instance=RequestContext(request))
