181d5798641d153652056b89c25423d1d6425979
Nick Reid <nickreid@nickreid.com>
2015-03-25 11:06:03 -0400

    overrode username display;

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def user_unicode(self):
+    if self.get_full_name():
+    	return self.get_full_name()
+    if self.email:
+    	return self.email
+    return  self.username
+
+User.__unicode__ = user_unicode
