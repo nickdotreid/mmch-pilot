69b5aac1685aea430b11792f63d67aa070846184
Nick Reid <nickreid@nickreid.com>
2015-03-28 12:18:31 -0400

    moved sms gateway to seperate url

--- sms/urls.py
+++ sms/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('sms.views',
+	url(r'^gateway/','gateway', name='sms_gateway'),
 	url(r'^register/(?P<number>.+)','register_input_pin', name='sms_input_pin'),
 	url(r'^register','register', name='sms_register'),
 	url(r'^terminal/(?P<number>.+)','terminal', name='sms_terminal'),
 	url(r'^terminal','terminal', name='sms_terminal'),
-	url(r'^','gateway', name='sms_gateway'),
 	)
